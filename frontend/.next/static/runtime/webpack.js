5d5778116cba3a1101c35199ec29bd7d4a4bbb27
fstepien <filip.stepien@gmail.com>
2019-04-13 06:53:37 -0400

    add styled components theme

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bf86b799701b6f1db339";
+/******/ 	var hotCurrentHash = "fd55c96c6259f48e8e2f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
